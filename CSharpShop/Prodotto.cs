19928b32bd22154630d72661d6d6ac8bb0652bf6
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-29 14:14:30 +0100

    fatto primi punti

---
+++ CSharpShop/Prodotto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop
+{
+    public class Prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private int prezzo;
+        private string iva;
+
+        
+        public Prodotto()
+        {
+
+        }
+        public Prodotto(int codice)
+        {
+            this.codice = codice;
+        }
+
+        public int getCodice()
+        {
+            return codice;
+        }
+
+
+
+
+    }
+}
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,1 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpShop;
+
+Random random= new Random();
+int numeroRandom = random.Next(0, 1000);
+
+Prodotto product1 = new Prodotto(numeroRandom);
+
+Console.WriteLine(product1);
+
+
+
