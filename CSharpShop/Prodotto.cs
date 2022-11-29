6cd71c302821c651a471d1a1209ac212aff841a4
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-29 14:42:40 +0100

    completato esercizio base

--- CSharpShop/Prodotto.cs
+++ CSharpShop/Prodotto.cs
@@ -10,13 +10,12 @@ namespace CSharpShop
 {
     public class Prodotto
     {
-        private int codice;
-        private string nome;
+        private int codice; 
+        private string nome = "prodotto";
         private string descrizione;
         private int prezzo;
-        private string iva;
+        private int iva;
 
-        
         public Prodotto()
         {
 
@@ -26,13 +25,58 @@ namespace CSharpShop
             this.codice = codice;
         }
 
-        public int getCodice()
+        public int GetCodice()
         {
             return codice;
         }
 
+        public string GetPrezzoSenzaIva()
+        {
+            return prezzo + "$";
+        }
 
+        public string GetDescrizione()
+        {
+            return descrizione;
+        }
 
+        public string GetNome()
+        {
+            return nome;
+        }
+       public string GetIva()
+        {
+            return iva + "%";
+        }
 
+        public void SetNome(string nome) {
+
+            this.nome = nome;
+        }
+
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione= descrizione;
+        }
+
+        public void SetPrezzo(int prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+
+        public void SetIva(int iva)
+        {
+            this.iva = iva;
+        }
+
+        public int GetPrezzoConIva()
+        {
+            return prezzo*iva/100 ;
+        }
+
+        public string GetNomeEsteso()
+        {
+            return nome + codice;
+        }
     }
 }
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -6,7 +6,22 @@ Random random= new Random();
 int numeroRandom = random.Next(0, 1000);
 
 Prodotto product1 = new Prodotto(numeroRandom);
+Console.WriteLine("nomeEsteso : " + product1.GetNomeEsteso());
+//adesso cambiare il nome:
+product1.SetNome("NewName");
+Console.WriteLine("il nome del prodotto esteso è: "+ product1.GetNomeEsteso());
+// set iva
+product1.SetIva(22);
+Console.WriteLine("iva del prodotto è:"+product1.GetIva());
+//set descrizione
+product1.SetDescrizione("Tv Cable");
 
+Console.WriteLine("descirizone del prodotto è: "+ product1.GetDescrizione());
+//set prezzo
+product1.SetPrezzo(23);
+
+Console.WriteLine("prezzo senza iva : " + product1.GetPrezzoSenzaIva());
+Console.WriteLine($"prezzo con iva è {product1.GetPrezzoConIva()}");
 
 
 
